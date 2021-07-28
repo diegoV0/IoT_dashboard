a278935da98791293e523923ea393d7eafffeea4
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2021-07-28 12:14:15 -0500

    model for device created

---
+++ api/middlewares/authentication.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+let checkAuth = (req, res, next) => {
+  let token = req.get("token");
+
+  jwt.verify(token, "securePassWordHere", (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        status: "error",
+        error: err
+      });
+    }
+    req.userData = decoded.userData;
+    next();
+  });
+};
+
+module.exports = { checkAuth };
