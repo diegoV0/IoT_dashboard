d5e15edc1544d743b14e20b751e2cb9b167c8e32
Diego Fernando Velesaca Orellana <diegovo@outlook.com>
2021-08-09 11:45:39 -0500

    delete and select devices ready

--- api/routes/devices.js
+++ api/routes/devices.js
@@ -51,12 +51,63 @@ router.post("/device", checkAuth, async (req, res) => {
   }
 });
 
-router.delete("/device", (req, res) => {});
+router.delete("/device", checkAuth, async (req, res) => {
+  try {
+    const userId = req.userData._id;
+    const dId = req.query.dId;
+    console.log(dId);
+    const result = await Device.deleteOne({ userId: userId, dId: dId });
+    const toSend = {
+      status: "success",
+      data: result
+    };
+    return res.json(toSend);
+  } catch (error) {
+    console.log("ERROR DELETING DEVICE");
+    console.log(error);
+    const toSend = {
+      status: "error",
+      error: error
+    };
+    return res.status(500).json(toSend);
+  }
+});
 
-router.put("/device", (req, res) => {});
+router.put("/device", (req, res) => {
+  const dId = req.body.dId;
+  console.log(req.userData._id);
+  if (selectDevice(userId, dId)) {
+    const toSend = {
+      status: "success"
+    };
+    return res.json(toSend);
+  } else {
+    const toSend = {
+      status: "error"
+    };
+    return res.json(toSend);
+  }
+});
 
 /*----------------------------------------------------------
                            Funtions
 -------------------------------------------------------------*/
+async function selectDevice(userId, dId) {
+  try {
+    const result = await Device.updateMany(
+      { userId: userId },
+      { selected: false }
+    );
+    const result2 = await Device.updateOne(
+      { dId: dId, userId: userId },
+      { selected: true }
+    );
+    return true;
+  } catch (error) {
+    console.log("ERROR IN 'selectDevice' FUNCTION ");
+    console.log(error);
+    return false;
+  }
+}
 
 module.exports = router;
